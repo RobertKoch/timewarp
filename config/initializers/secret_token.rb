d143e4e4d00c710a4259ed045c4510d6f5a085fa
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-01-24 18:02:01 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timewarp::Application.config.secret_token = '35aa62c02baa9a6b0be658f7f214f23b1e0a81ba34a7a900fcfa0a5c8142fa2aaaf568c41eb08e6d838bb1dd4a60007ed7c3b1a991858bf530d7d15ce1c973d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Timewarp::Application.config.session_store :cookie_store, key: '_timewarp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Timewarp::Application.config.session_store :active_record_store
