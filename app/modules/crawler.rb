29d2f593854847bb10784c920b201b2f7b0e74a5
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-03-05 19:24:47 +0100

    crawler saves sites and create folders now, save files in tmp directory

--- app/modules/crawler.rb
+++ app/modules/crawler.rb
@@ -1,23 +1,24 @@
 class Crawler
   def initialize(site)
     @site = site
-    @site_dir = Settings.crawler.sites_folder_path+"/#{@site.token}"
+    @site_dir = Rails.root.join(Settings.crawler.sites_folder_path).join(@site.token)
   end
 
   def crawl_site
+    #create folders and save site to current
+    create_folders
+    system("wget --convert-links --force-html --output-document=#{@site_dir}/current/index.html #{@site.url}")
   end
 
   def get_site_title
   end
 
   def create_folders
-    #create public/sites if not existing
-    sites_dir = Settings.crawler.sites_folder_path
-    Dir.mkdir(sites_dir) unless File.exists?(sites_dir)
-
-    if Dir.mkdir(@site_dir)
+    # using FileUtils allows to create dirs progressively
+    # so it is not necessary to check if dirs are existing already
+    if FileUtils.mkdir_p @site_dir
       Settings.crawler.years.each do |foldername|
-        Dir.mkdir(@site_dir+"/#{foldername}")
+        Dir.mkdir(@site_dir.join(foldername))
       end
     end
   end
