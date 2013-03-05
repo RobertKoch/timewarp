37584308d33a538458c419f1a16fdf2df8ba55db
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-03-05 13:34:01 +0100

    use less for css

--- Gemfile
+++ Gemfile
@@ -10,7 +10,8 @@ gem 'rails', '3.2.12'
 gem 'mongoid'
 
 gem 'haml-rails'
-
+gem 'less-rails'
+gem 'therubyracer'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Timewarp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #less
+  config.less.paths << "#{Rails.root}/lib/less/protractor/stylesheets"
+  config.less.compress = true
 end
