3f75f0012d504f0acc75f36934d4db24ccd41d93
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-05-22 11:37:08 +0200

    add user-agent (Chrome, Linux) to wget command

--- app/modules/crawler.rb
+++ app/modules/crawler.rb
@@ -7,10 +7,10 @@ class Crawler
   def crawl_site
     #create folders and save site to current
     create_folders
-    wget = system("wget --convert-links --force-html --output-document=#{@site_dir}/current/index.html #{@site.url}")
+    wget = system("wget -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31' --convert-links --force-html --output-document=#{@site_dir}/current/index.html #{@site.url}")
     
     #this would crawl site with all assets
-    #wget = system("wget --page-requisites --convert-links --directory-prefix='#{@site_dir}/current/' --force-html --html-extension -e robots=off #{@site.url}")
+    #wget = system("wget -U 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31' --page-requisites --convert-links --directory-prefix='#{@site_dir}/current/' --force-html --html-extension -e robots=off #{@site.url}")
     
     wget
   end
