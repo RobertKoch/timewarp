a9d46c77453ae25176f1489f3c4c80234a33ba53
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-05-27 12:16:06 +0200

    route for preview

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Timewarp::Application.routes.draw do
   get "tag_search" => "tags#search", :as => "tag_search"
 
   get "archive" => "sites#index", :as => "archive"
+  get "sites/:id/preview" => "sites#preview", :as => "sites_preview" #only for admins
   get "sites/:id/analyse" => "sites#analyse", :as => "sites_analyse"
   get "sites/:id/timeline" => "sites#timeline", :as => "sites_timeline"
   put "sites/:id/publish" => "sites#publish", :as => "sites_publish"
