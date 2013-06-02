f45a3fce73bb48fa30b697383e5010c869c286e8
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-06-02 22:28:07 +0200

    mobile detection for home view and dummy text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,16 @@ class ApplicationController < ActionController::Base
     end
     taglist
   end
+
+private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  helper_method :mobile_device?
 end
