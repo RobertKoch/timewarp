6ffc9c3481a2835cf9c468890f463aef01841497
Robert Koch <rkoch.mmt-b2010@fh-salzburg.ac.at>
2013-06-04 15:31:45 +0200

    reload data with seed data after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,11 @@ namespace :deploy do
   task :copy_config do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
   end
+
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
 end
 
 require "bundler/capistrano"
